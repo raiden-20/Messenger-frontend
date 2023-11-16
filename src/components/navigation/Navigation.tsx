2e26b1edb1a90ad01d279de387149970fed709bb
korobasssss <112711112+korobasssss@users.noreply.github.com>
2023-11-17 00:42:41 +0300

    made navigation layout

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,8 +1,47 @@
-import React from "react";
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import nav from './Navigation.module.css';
+
+import simon from '../../assets/images/simon.svg'
+import profile from '../../assets/images/icons_navigation/profile.svg'
+import dialogs from '../../assets/images/icons_navigation/dialogs.svg'
+import friends from '../../assets/images/icons_navigation/friends.svg'
+import search from '../../assets/images/icons_navigation/search.svg'
+import settings from '../../assets/images/icons_navigation/settings.svg'
 
 const Navigation = () => {
     return (
-        <div>
+        <div className={nav.page}>
+            <header>
+                <img src={simon}/>
+                СимОн
+            </header>
+            <section className={nav.links}>
+                <section className={nav.svg_link}>
+                    <img src={profile}/>
+                    <NavLink className={nav.button_nav} to={"/profile"}>Профиль</NavLink>
+                </section>
+                <section className={nav.svg_link}>
+                    <img src={dialogs}/>
+                    <NavLink className={nav.button_nav} to={"/dialogs"}>Сообщения</NavLink>
+                </section>
+                <section className={nav.svg_link}>
+                    <img src={friends}/>
+                    <NavLink className={nav.button_nav} to={"/friends"}>Друзья</NavLink>
+                </section>
+                <section className={nav.svg_link}>
+                    <img src={search}/>
+                    <NavLink className={nav.button_nav} to={"/search"}>Поиск</NavLink>
+                </section>
+                <section className={nav.svg_link}>
+                    <img src={settings}/>
+                    <NavLink className={nav.button_nav} to={"/settings"}>Настройки</NavLink>
+                </section>
+                <section className={nav.svg_link}>
+                    <section></section>
+                    <NavLink className={nav.button_nav} to={"/settings"}>Еще</NavLink>
+                </section>
+            </section>
 
         </div>
     )
