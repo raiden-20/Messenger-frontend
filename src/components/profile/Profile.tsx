cdd35f43d4948fc06fe3ca7b2df3fdf8583812c9
korobasssss <112711112+korobasssss@users.noreply.github.com>
2023-11-17 23:33:22 +0300

    intermediate commit profile page posts (i will fix it)

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -4,6 +4,7 @@ import prof from './Profile.module.css'
 import cat from '../../assets/images/cat_registration.jpg'
 import create from '../../assets/images/other/create_post.svg'
 import profile_settings from '../../assets/images//other/profile_settings.svg'
+import post_css from "./post/Post.module.css";
 
 const Profile = () => {
     return (
@@ -34,6 +35,35 @@ const Profile = () => {
                                 </section>
                             </section>
                             <section className={prof.posts}>
+                                <section className={post_css.all_page}>
+                                    <section className={post_css.page}>
+                                        <header>
+                                            <section>
+
+                                            </section>
+                                            <section className={post_css.name_and_status}>
+                                                <div className={post_css.name}>Имя Фамилия</div>
+                                                <div className={post_css.userName}>@name</div>
+                                            </section>
+                                        </header>
+                                        <section className={post_css.post}>
+                                            <section className={post_css.photo_post}>
+                                                <section>
+
+                                                </section>
+                                                <section>
+
+                                                </section>
+                                            </section>
+                                            <section>
+                                                Lorem ipsum dolor sit amet consectetur. Cras facilisis diam lacus massa ultricies volutpat vitae. Natoque consequat ut id nulla a faucibus scelerisque in. Ullamcorper dignissim blandit enim sed morbi urna sit. Amet interdum amet purus urna sit. Odio interdum nec elit arcu nunc. Posuere bibendum tempor adipiscing aliquet dignissim. In arcu viverra id suspendisse amet.
+                                            </section>
+                                            <footer>
+
+                                            </footer>
+                                        </section>
+                                    </section>
+                                </section>
                             </section>
                         </main>
                     </section>
